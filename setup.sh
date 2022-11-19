46ba1c50be92ad486753895c1ba9ffeaef11b0cd
uahmad417 <18u.ahmad@gmail.com>
2022-11-19 12:19:46 +0500

    added condition for docker install. now the user will be asked if he want to install docker engine

--- setup.sh
+++ setup.sh
@@ -18,32 +18,34 @@ sudo apt-get install -y \
 echo "Installing essential python modules"
 pip install virtualenv \
     bpython > /dev/null
-echo "Installing Docker"
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release > /dev/null
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update > /dev/null
-sudo apt-get install \
-    docker-ce \
-    docker-ce-cli \
-    containerd.io \
-    docker-compose-plugin > /dev/null
-echo "Docker Installed"
-echo "Starting Docker Service"
-sudo service docker start
-echo "Testing Docker"
-sudo docker run hello-world > /dev/null
-if [ $? -eq 0 ]; then
-    echo "Docker is working"
-else
-    echo "Something went wrong with setting up Docker"
+if [ "$DOCKER" = "y" ]; then
+    echo "Installing Docker"
+    sudo apt-get install \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release > /dev/null
+    mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update > /dev/null
+    sudo apt-get install \
+        docker-ce \
+        docker-ce-cli \
+        containerd.io \
+        docker-compose-plugin > /dev/null
+    echo "Docker Installed"
+    echo "Starting Docker Service"
+    sudo service docker start
+    echo "Testing Docker"
+    sudo docker run hello-world > /dev/null
+    if [ $? -eq 0 ]; then
+        echo "Docker is working"
+    else
+        echo "Something went wrong with setting up Docker"
+    fi
 fi
 echo "Setting up oh-my-fish shell prompt"
 echo "Installing fish"
